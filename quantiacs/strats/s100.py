7d65eaa1a75b0e13493d9372b8b70d39197497ed
yfushimi@neospace.com <yfushimi@neospace.com>
2015-10-24 14:15:36 +0900

    hi

--- quantiacs/strats/s100.py
+++ quantiacs/strats/s100.py
@@ -1,50 +1,121 @@
-'''
 ### Quantiacs Mean Reversion Trading System Example
 # import necessary Packages below:
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 bars=set();
+symbols=('F_CL','F_NG');
+sym0_idx=1;
+sym1_idx=2;
+
+pairs=pd.DataFrame({}, columns=['datetime','open','high','low','close','volume','na']);
+#datadir = 'tickerData'  # Change this to reflect your data path!
+#pairs = create_pairs_dataframe(datadir, symbols)
+
+def create_pairs_dataframe(datadir, symbols):
+    """Creates a pandas DataFrame containing the closing price
+    of a pair of symbols based on CSV files containing a datetime
+    stamp and OHLCV data."""
+
+    # Open the individual CSV files and read into pandas DataFrames
+    print "Importing CSV data..."
+    sym1 = pd.io.parsers.read_csv(os.path.join(datadir, '%s.txt' % symbols[0]),
+                                  header=0, index_col=0,
+                                  names=['datetime','open','high','low','close','volume','na'])
+    sym2 = pd.io.parsers.read_csv(os.path.join(datadir, '%s.txt' % symbols[1]),
+                                  header=0, index_col=0,
+                                  names=['datetime','open','high','low','close','volume','na'])
+
+    # Create a pandas DataFrame with the close prices of each symbol
+    # correctly aligned and dropping missing entries
+    print "Constructing dual matrix for %s and %s..." % symbols
+    pairs = pd.DataFrame(index=sym1.index)
+    pairs['%s_close' % symbols[0].lower()] = sym1['close']
+    pairs['%s_close' % symbols[1].lower()] = sym2['close']
+    pairs = pairs.dropna()
+    return pairs
+
+def calculate_spread_zscore(pairs, symbols, lookback=100):
+    """Creates a hedge ratio between the two symbols by calculating
+    a rolling linear regression with a defined lookback period. This
+    is then used to create a z-score of the 'spread' between the two
+    symbols based on a linear combination of the two."""
+
+    # Use the pandas Ordinary Least Squares method to fit a rolling
+    # linear regression between the two closing price time series
+    print "Fitting the rolling Linear Regression..."
+    model = pd.ols(y=pairs['%s_close' % symbols[0].lower()],
+                   x=pairs['%s_close' % symbols[1].lower()],
+                   window=lookback)
+
+    # Construct the hedge ratio and eliminate the first
+    # lookback-length empty/NaN period
+    pairs['hedge_ratio'] = model.beta['x']
+    pairs = pairs.dropna()
+
+    # Create the spread and then a z-score of the spread
+    print "Creating the spread/zscore columns..."
+    pairs['spread'] = pairs['%s_close' % symbols[0].lower()] - pairs['hedge_ratio']*pairs['%s_close' % symbols[1].lower()]
+    pairs['zscore'] = (pairs['spread'] - np.mean(pairs['spread']))/np.std(pairs['spread'])
+    return pairs
+
 ##### Do not change this function definition #####
-def myTradingSystem(DATE, CLOSE, settings):
+def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings):
+    global pairs;
+    global symbols;
     # This system uses mean reversion techniques to allocate capital into the desired equities
 
     # This strategy evaluates two averages over time of the close over a long/short
     # scale and builds the ratio. For each day, "smaQuot" is an array of "nMarkets"
     # size.
-    nMarkets = numpy.shape(CLOSE)[1]
-    if nMarkets == 'F_CL':
-    	bars['instr1']=CLOSE;
-    if nMarkets == 'F_NG':
-	bars['instr2']=CLOSE;
-    if bars['instr1'] is not None and bars['instr2'] is not None:
-    	dblRatioData = bars['instr1'][0,:]; / bars['instr2'][0,:];
-        tsPairratio.Add(bar1.DateTime, dblRatioData);
-        dblRatioData2 = bars['instr2'][0,:] / bars['instr1'][0,:];
-        tsPairratio2.Add(bar2.DateTime, dblRatioData2);
+    nMarkets = np.shape(CLOSE)[1]
+    closePrices=pd.DataFrame(CLOSE, columns=settings['markets']); 
+    pairs['%s_close' % symbols[0].lower()] = closePrices[symbols[0]]; #np.take(CLOSE, sym0_idx, axis=1);
+    pairs['%s_close' % symbols[1].lower()] = closePrices[symbols[1]]; #np.take(CLOSE, sym1_idx, axis=1);
+    pairs = calculate_spread_zscore(pairs, symbols, 200)
+    z_entry_threshold=2.0
+    z_exit_threshold=1.0
 
-    periodLong= 200
-    periodShort= 40
+    # Calculate when to be long, short and when to exit
+    pos= np.zeros((1,nMarkets))
+    if 'zscore' not in pairs.index or pairs['zscore'] is None:
+	return pos, settings;
 
-    smaLong=   numpy.sum(CLOSE[-periodLong:,:], axis=0)/periodLong
-    smaRecent= numpy.sum(CLOSE[-periodShort:,:],axis=0)/periodShort
-    smaQuot= smaRecent / smaLong
+    if pairs['zscore'] <= -z_entry_threshold:
+    	pos[0,sym0_idx] = 1
+    	pos[0,sym1_idx] = -1
+
+    elif pairs['zscore'] >= z_entry_threshold:
+	pos[0,sym0_idx] = -1
+        pos[0,sym1_idx] = 1
+    elif np.abs(pairs['zscore']) <= z_exit_threshold:
+	 pos[0,sym0_idx] = 0
+         pos[0,sym1_idx] = 0 
+
+    #periodLong= 200
+    #periodShort= 40
+
+    #smaLong=   np.sum(CLOSE[-periodLong:,:], axis=0)/periodLong
+    #smaRecent= np.sum(CLOSE[-periodShort:,:],axis=0)/periodShort
+    #smaQuot= smaRecent / smaLong
 
     # For each day, scan the ratio of moving averages over the markets and find the
     # market with the maximum ratio and the market with the minimum ratio:
-    longEquity = numpy.where(smaQuot == numpy.nanmin(smaQuot))
-    shortEquity= numpy.where(smaQuot == numpy.nanmax(smaQuot))
+    #longEquity = np.where(smaQuot == np.nanmin(smaQuot))
+    #shortEquity= np.where(smaQuot == np.nanmax(smaQuot))
 
     # Take a contrarian view, going long the market with the minimum ratio and
     # going short the market with the maximum ratio. The array "pos" will contain
     # all zero entries except for those cases where we go long (1) and short (-1):
-    pos= numpy.zeros((1,nMarkets))
-    pos[0,longEquity[0][0]] = 1
-    pos[0,shortEquity[0][0]]= -1
+    #pos= np.zeros((1,nMarkets))
+    #pos[0,longEquity[0][0]] = 1
+    #pos[0,shortEquity[0][0]]= -1
 
     # For the position sizing, we supply a vector of weights defining our
     # exposure to the markets in settings['markets']. This vector should be
     # normalized.
-    pos= pos/numpy.nansum(abs(pos))
+    pos= pos/np.nansum(abs(pos))
 
     return pos, settings
 
@@ -66,9 +137,9 @@ def mySettings():
     # 'UPS','USB','UTX','V','VZ','WAG','WFC','WMT','XOM']
 
     # Futures Contracts
-    settings['markets']  = ['CASH','F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CD',  \
-    'F_CL', 'F_DJ', 'F_EC', 'F_ES', 'F_FV', 'F_GC', 'F_HG', 'F_HO', 'F_LC', \
-    'F_LN', 'F_NG', 'F_NQ', 'F_RB', 'F_S', 'F_SF', 'F_SI', 'F_SM', 'F_SP', \
+    settings['markets']  = ['CASH','F_CL','F_NG', 'F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CD',  \
+    'F_DJ', 'F_EC', 'F_ES', 'F_FV', 'F_GC', 'F_HG', 'F_HO', 'F_LC', \
+    'F_LN', 'F_NQ', 'F_RB', 'F_S', 'F_SF', 'F_SI', 'F_SM', 'F_SP', \
     'F_TY', 'F_US', 'F_W', 'F_YM']
 
 
@@ -78,72 +149,3 @@ def mySettings():
 
     return settings
 
-'''
-
-### Quantiacs Mean Reversion Trading System Example
-# import necessary Packages below:
-import numpy
-
-##### Do not change this function definition #####
-def myTradingSystem(DATE, CLOSE, settings):
-    ''' This system uses mean reversion techniques to allocate capital into the desired equities '''
-
-    # This strategy evaluates two averages over time of the close over a long/short
-    # scale and builds the ratio. For each day, "smaQuot" is an array of "nMarkets"
-    # size.
-    nMarkets = numpy.shape(CLOSE)[1]
-    periodLong= 200
-    periodShort= 40
-
-    smaLong=   numpy.sum(CLOSE[-periodLong:,:], axis=0)/periodLong
-    smaRecent= numpy.sum(CLOSE[-periodShort:,:],axis=0)/periodShort
-    smaQuot= smaRecent / smaLong
-
-    # For each day, scan the ratio of moving averages over the markets and find the
-    # market with the maximum ratio and the market with the minimum ratio:
-    longEquity = numpy.where(smaQuot == numpy.nanmin(smaQuot))
-    shortEquity= numpy.where(smaQuot == numpy.nanmax(smaQuot))
-
-    # Take a contrarian view, going long the market with the minimum ratio and
-    # going short the market with the maximum ratio. The array "pos" will contain
-    # all zero entries except for those cases where we go long (1) and short (-1):
-    pos= numpy.zeros((1,nMarkets))
-    pos[0,longEquity[0][0]] = 1
-    pos[0,shortEquity[0][0]]= -1
-
-    # For the position sizing, we supply a vector of weights defining our
-    # exposure to the markets in settings['markets']. This vector should be
-    # normalized.
-    pos= pos/numpy.nansum(abs(pos))
-
-    return pos, settings
-
-
-##### Do not change this function definition #####
-def mySettings():
-    ''' Define your trading system settings here '''
-    settings= {}
-
-    # S&P 100 stocks
-    # settings['markets']=['CASH','AAPL','ABBV','ABT','ACN','AEP','AIG','ALL', \
-    # 'AMGN','AMZN','APA','APC','AXP','BA','BAC','BAX','BK','BMY','BRKB','C', \
-    # 'CAT','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DIS','DOW',\
-    # 'DVN','EBAY','EMC','EMR','EXC','F','FB','FCX','FDX','FOXA','GD','GE', \
-    # 'GILD','GM','GOOGL','GS','HAL','HD','HON','HPQ','IBM','INTC','JNJ','JPM', \
-    # 'KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MON', \
-    # 'MRK','MS','MSFT','NKE','NOV','NSC','ORCL','OXY','PEP','PFE','PG','PM', \
-    # 'QCOM','RTN','SBUX','SLB','SO','SPG','T','TGT','TWX','TXN','UNH','UNP', \
-    # 'UPS','USB','UTX','V','VZ','WAG','WFC','WMT','XOM']
-
-    # Futures Contracts
-    settings['markets']  = ['CASH','F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CD',  \
-    'F_CL', 'F_DJ', 'F_EC', 'F_ES', 'F_FV', 'F_GC', 'F_HG', 'F_HO', 'F_LC', \
-    'F_LN', 'F_NG', 'F_NQ', 'F_RB', 'F_S', 'F_SF', 'F_SI', 'F_SM', 'F_SP', \
-    'F_TY', 'F_US', 'F_W', 'F_YM']
-
-
-    settings['lookback']= 504
-    settings['budget']= 10**6
-    settings['slippage']= 0.05
-
-    return settings
